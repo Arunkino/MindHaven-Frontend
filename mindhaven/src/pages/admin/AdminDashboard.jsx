17a26856dc40d1b57b3e42a75b1eb730a58d6e29
Arunkino <150350015+Arunkino@users.noreply.github.com>
2024-09-14 08:47:47 +0530

    Update AdminDashboard.jsx
    certifiate image is not loading. changed baseurl

--- mindhaven/src/pages/admin/AdminDashboard.jsx
+++ mindhaven/src/pages/admin/AdminDashboard.jsx
@@ -4,7 +4,8 @@ import { fetchApprovedMentors, fetchPendingMentors, fetchUsers, approveMentor, r
 import FullScreenImage from '../../components/FullScreenImage';
 
 
-const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+// const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+const BASE_URL = 'http://api.mindhaven.site';
 const AdminDashboard = () => {
   const [activeTab, setActiveTab] = useState('pending-mentors');
   const [pendingMentors, setPendingMentors] = useState([]);
